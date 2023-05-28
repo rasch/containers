5761f117ba08549d54613fff0112233c49c4b3cb
Randy Schneck <i@rasch.co>
2023-05-28 15:10:45 -0400

    🚨 Remove trailing whitespace

--- build/examples/hello.m
+++ build/examples/hello.m
@@ -3,6 +3,6 @@
 int main (int argc, const char * argv[]) {
   NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
   NSLog (@"Hello, World!");
-  [pool drain];    
-  return 0; 
-} 
+  [pool drain];
+  return 0;
+}
